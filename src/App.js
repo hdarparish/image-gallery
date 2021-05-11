9e0e0711e05b505e6cb52abd8f358d12fc9744a7
Darparish <hosseindarp@gmail.com>
2021-05-11 14:01:37 -0400

    inital setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//styles
+import GlobalStyles from "./components/GlobalStyles";
+//pages and components
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
